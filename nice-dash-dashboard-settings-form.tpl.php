868d4913bca10efdd57399615b0b48f672d3d12c
kristofvanroy <vanroy.kristof@gmail.com>
2010-04-12 15:43:22 +0200

    Refactored module + added drag and drop support

---
+++ nice-dash-dashboard-settings-form.tpl.php
@@ -0,0 +1,47 @@
+<?php
+if ($rows): ?>
+<div id="ds-display-content">
+
+<div id="field-content">
+  <table id="fields" class="sticky-enabled">
+    <thead>
+      <tr>
+        <th><?php print t('Widget'); ?></th>
+        <th><?php print t('Region'); ?></th>
+        <th><?php print t('Weight'); ?></th>
+      </tr>
+    </thead>
+    <tbody>
+
+    <!-- Node regions -->
+    <?php foreach ($regions as $region => $title): ?>
+      <tr class="region region-<?php print $region?>">
+        <td colspan="6" class="region"><?php print $title; ?></td>
+      </tr>
+      <tr class="region-message region-<?php print $region?>-message <?php print empty($rows[$region]) ? 'region-empty' : 'region-populated'; ?>">
+        <td colspan="6"><em><?php print t('No widgets in this region'); ?></em></td>
+      </tr>
+
+      <!-- fields -->
+      <?php
+      if (!empty($rows[$region])):
+        $count = 0;
+        foreach ($rows[$region] as $row):?>
+          <tr class="<?php print $count % 2 == 0 ? 'odd' : 'even'; ?> <?php print $row->class ?>">
+            <td><?php print $row->title; ?></td>
+            <td><?php print $row->region; ?></td>
+            <td><?php print $row->weight; ?></td>
+          </tr>
+          <?php
+          $count++;
+        endforeach;
+      endif;
+    endforeach;
+    ?>
+    </tbody>
+  </table>
+</div>
+</div>
+<?php
+print $submit;
+endif;
