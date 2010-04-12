447d648002d279d1e45c648e610298578ee7c2ce
kristofvanroy <vanroy.kristof@gmail.com>
2010-04-12 17:11:06 +0200

    Added template files for Dashboard and Dashboard Components

---
+++ theme/nice-dash-dashboard-component.tpl.php
@@ -0,0 +1,5 @@
+<div class="dashboard-component admin-panel">
+<h3><strong><?php print $title; ?></strong></h3>
+<p class="description"><?php print $description; ?></p>
+<div class="content"><?php print $content; ?></div>
+</div>
---
+++ theme/nice-dash-dashboard.tpl.php
@@ -0,0 +1,6 @@
+<div id="nice-dashboard">
+  <div><?php print $regions['header']; ?></div>
+  <div class="left"><?php print $regions['left']; ?></div>
+  <div class="right"><?php print $regions['right']; ?></div>
+  <div class="clear"><?php print $regions['footer']; ?></div>
+</div>
