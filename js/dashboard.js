34b6248c8a061c46c8f95309a010e202468235ee
kristofvanroy <vanroy.kristof@gmail.com>
2010-04-13 13:09:52 +0200

    Added slide open support for views exposed filters

---
+++ js/dashboard.js
@@ -0,0 +1,16 @@
+Drupal.NiceDash = Drupal.NiceDash || {};
+$(document).ready(function(){
+  Drupal.NiceDash();
+});
+Drupal.NiceDash = function(){
+  initFilters();
+  function initFilters(){
+    $('.view-filters').parent().before('<div class="filter-container"><a href="" class="toggle-link">Toggle filter</a></div><br class="clear" />');
+    $('.view-filters').toggle();
+    $('.toggle-link').bind('click', toggleFilters);
+  }
+  function toggleFilters(e){
+    $(e.target).parent().parent().find('.view-filters').slideToggle();
+    e.preventDefault();
+  }
+}
