f112384c1343b8f7ad6a493548dfe44acd30075c
Stijn De Meyere <stijn@sken.be>
2010-06-03 23:43:33 +0800

    Making custom widget description optional

--- theme/nice-dash-dashboard-component.tpl.php
+++ theme/nice-dash-dashboard-component.tpl.php
@@ -1,5 +1,10 @@
 <div class="dashboard-component admin-panel">
-<h3><strong><?php print $title; ?></strong></h3>
-<p class="description"><?php print $description; ?></p>
-<div class="content"><?php print $content; ?></div>
+  <h3><strong><?php print $title; ?></strong></h3>
+  <?php if ($description): ?>
+    <p class="description"><?php print $description; ?></p>
+  <?php endif; ?>
+
+  <div class="content">
+    <?php print $content; ?>
+  </div>
 </div>
