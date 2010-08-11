e242e17f20e6547610da04350918865dedb42514
Kristof De Jaeger <swentel@swentelbox.localdomain>
2010-08-11 11:54:48 +0200

    Plugins enable/disable and component minimizer

--- theme/nice-dash-dashboard-component.tpl.php
+++ theme/nice-dash-dashboard-component.tpl.php
@@ -6,13 +6,15 @@
  */
 ?>
 
-<div class="dashboard-component admin-panel">
-  <h3><strong><?php print $title; ?></strong></h3>
-  <?php if ($description): ?>
-    <p class="description"><?php print $description; ?></p>
-  <?php endif; ?>
+<div id="<?php print $ctools_id; ?>" class="dashboard-component admin-panel ctools-collapsible-container ctools-collapsible-remember">
+  <div class="ctools-collapsible-handle">
+    <h3><strong><?php print $title; ?></strong></h3>
+    <?php if ($description): ?>
+      <p class="description"><?php print $description; ?></p>
+    <?php endif; ?>
+  </div>
 
-  <div class="content">
+  <div class="content ctools-collapsible-content">
     <?php print $content; ?>
   </div>
 </div>
